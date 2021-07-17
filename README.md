39080f1f7a980bd1d5677f7537cab30b904f1dee
kelvintaywl <kelvintaywl@gmail.com>
2021-07-17 23:45:46 +0900

    feat: implement skeletal app

---
+++ README.md
@@ -0,0 +1,18 @@
+# MIN
+
+This is my attempt at building a Single Page Application (SPA) with [Hyperapp](https://github.com/jorgebucaran/hyperapp).
+
+It's been a while since I built any web frontend or UI.
+
+This should be fun! :tumbler:
+
+## Prerequisites
+
+- [nvm, for installing specific Node runtime version](https://github.com/nvm-sh/nvm)
+- [Node, see `.nvmrc` for version details](.nvmrc)
+
+
+## Local setup
+
+
+### Testing
---
+++ __tests__/index.test.js
@@ -0,0 +1,40 @@
+// NOTE: these tests assume we have the app served locally at port 5000.
+const serverURL = "http://localhost:5000";
+
+describe("index page", () => {
+  beforeAll(async () => {
+    await page.goto(serverURL, { waitUntil: "domcontentloaded" });
+  });
+
+  it('should be titled "MIN"', async () => {
+    const title = await page.title();
+    expect(title).toEqual("MIN");
+  });
+
+  it("should have a button with label", async () => {
+    const button = await page.$("button");
+    const button_text = await (
+      await button.getProperty("textContent")
+    ).jsonValue();
+    expect(button_text).toMatch(/import this$/);
+  });
+
+  it("should have a empty pre element at start", async () => {
+    const pre = await page.$("#pre-display");
+    const pre_text = await (await pre.getProperty("innerHTML")).jsonValue();
+    expect(pre_text).toEqual("");
+  });
+
+  it("should insert quote in pre when button is clicked", async () => {
+    const button = await page.$("button");
+
+    // when
+    await button.evaluate((button) => button.click());
+
+    // then
+    const pre = await page.$("#pre-display");
+
+    const pre_text = await (await pre.getProperty("innerHTML")).jsonValue();
+    expect(pre_text).toMatch(/The Zen of Python, by Tim Peters/);
+  });
+});
